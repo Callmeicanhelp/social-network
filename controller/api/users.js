ece1f9b3d507363107043ca3600f9227a9c69510
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-09-03 00:00:04 -0400

    rewrote user routes

--- controller/api/users.js
+++ controller/api/users.js
@@ -1,20 +1,30 @@
 const { User } = require("../../models");
 const router = require("express").Router();
 
-router
-  .route("/")
-  .get((req, res) => {
-    User.find({})
-      // .populate({path: 'thoughts', select: '-__v'})
-      // .populate({path: 'friends', select: '-__v'})
-      .select("-__v")
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  })
-  .post(({ body }, res) => {
+// Get all users
+router.route("/").get((req, res) => {
+  User.find({})
+    .select("-__v")
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}),
+  //Get user by id
+  router.route("/:id").get(({ params }, res) => {
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "This user id does not exist" });
+          return;
+        }
+        res.status(200).json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  }),
+  // Add new user
+  router.post(({ body }, res) => {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
@@ -22,75 +32,64 @@ router
         res.status(400).json(err);
       });
   }),
-  // const userController = {
-  //   getAllUsers(req, res) {
-  //     User.find({})
-  //       .then((dbUserData) => res.json(dbUserData))
-  //       .catch((err) => {
-  //         console.log(err);
-  //         res.status(400).json(err);
-  //       });
-  //   },
+  //Update a user
+  router.put(({ params, body }, res) => {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "This user id does not exist" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  }),
+  // Delete a user
+  router.delete(({ params }, res) => {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "This user id does not exist" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  }),
+  // Add friend
+  router.put(({ params }, res) => {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "This user id does not exist" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  }),
+  // Delete friend
+  router.delete(({ params }, res) => {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "This user id does not exist" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  });
 
-  //   // GET a user by its ID
-  //   getUserById({ params }, res) {
-  //     User.find({ _id: params.id })
-  //       .then((dbUserData) => {
-  //         // If no user is found, send 404
-  //         if (!dbUserData) {
-  //           res.status(404).json({
-  //             message: "No user found with this id. PLease try again!",
-  //           });
-  //           return;
-  //         }
-  //         res.json(dbUserData);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //         res.status(400).json(err);
-  //       });
-  //   },
-
-  //   //POST a new user
-  //   postUser({ body }, res) {
-  //     User.create(body)
-  //       .then((dbUserData) => res.json(dbUserData))
-  //       .catch((err) => res.status(400).json(err));
-  //   },
-
-  //   // PUT to update user by its ID
-  //   updateUser({ params }, res) {
-  //     User.findOne({ _id: params.id })
-  //       .then((dbUserData) => {
-  //         // If user does not exist, display error message
-  //         if (!dbUserData) {
-  //           res.status(404).json({
-  //             message: "No user found with this id. Please try again!",
-  //           });
-  //           return;
-  //         }
-  //         res.json(dbUserData);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //         res.status(400).json(err);
-  //       });
-  //   },
-
-  //   //DELETE a user
-  //   deleteUser({ params }, res) {
-  //     User.findOneAndDelete({ _id: params.id })
-  //       .then((dbUserData) => {
-  //         if (!dbUserData) {
-  //           res.status(404).json({
-  //             message: "No user found with this id. Please try again!",
-  //           });
-  //           return;
-  //         }
-  //         res.json(dbUserData);
-  //       })
-  //       .catch((err) => res.status(400).json(err));
-  //   },
-  // };
-
-  (module.exports = router);
+module.exports = router;

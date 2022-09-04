f25b7bda6df5c0d98d9f5398f7a1a85d35c61a34
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-09-04 15:51:21 -0400

    still trying to make the routes work.

---
+++ controller/thought-controller.js
@@ -0,0 +1,149 @@
+const { Thought, User } = require("../../models");
+const router = require("express").Router();
+
+// Get all thoughts
+router.get("/", (req, res) => {
+  Thought.find({})
+    .populate({
+      path: "reaction",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbThoughtData) => res.json(dbThoughtData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+// Get thought by id
+router.route("/:id").get(({ params }, res) => {
+  Thought.findOne({ _id: params.id })
+    .populate({
+      path: "reaction",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(500).json({
+          message: "This thought id does not exist",
+        });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+// Post new thought
+router.post(({ params, body }, res) => {
+  console.log(User);
+  console.log(Thought);
+  Thought.create(body).then(({ _id }) => {
+    console.log(params);
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { thought: _id } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        console.log(dbThoughtData);
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "This thought id does not exist" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  });
+});
+//Update thought
+router.put(({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    {
+      _id: params.id,
+    },
+    body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate({
+      path: "reaction",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({
+          message: "This thought id does not exist",
+        });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.json(err));
+});
+// Delete thought
+router.delete(({ params }, res) => {
+  Thought.findOneAndDelete({ _id: params.id })
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "This thought id does not exist" });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.json(err));
+});
+// Add reaction to thought
+router.route("/:thoughtId/reactions").post(({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    {
+      _id: params.thoughtId,
+    },
+    { $push: { reactions: body } },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate({
+      path: "reactions",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(500).json({
+          message: "This thought id does not exist",
+        });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+//Update reaction
+router.route("/:thoughtId/reaction/:reactionID").delete(({ params }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $pull: { reaction: { reactionId: params.reactionId } } },
+    { new: true }
+  )
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "This thought id does not exist" });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.status(400).json(err));
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(require("./controller"));
+app.use(require("./routes"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
